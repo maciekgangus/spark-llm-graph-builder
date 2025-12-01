13908286e728e26db03ba6efad2104ed53d2341d
Maciej Lesniak <lesniakmaciek9040@gmail.com>
2025-12-01 11:10:21 +0100

    added config

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
-from pydantic import BaseSettings, Field
+from pydantic import Field
+from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     # These attributes will automatically look for matching keys
