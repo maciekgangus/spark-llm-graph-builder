0fd6f3411f2625039886f048ddc8d7aa25e487a6
Maciej Lesniak <lesniakmaciek9040@gmail.com>
2025-12-01 10:57:00 +0100

    added config

---
+++ config.py
@@ -0,0 +1,14 @@
+from pydantic import BaseSettings, Field
+
+class Settings(BaseSettings):
+    # These attributes will automatically look for matching keys
+    # in the environment (e.g., os.environ) and the .env file.
+    endpoint_url: str = Field(..., env='ENDPOINT_URL')
+    s3_username: str = Field(..., env='S3_USERNAME')
+    s3_password: str = Field(..., env='S3_PASSWORD')
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = 'utf-8'
+
+settings = Settings()
